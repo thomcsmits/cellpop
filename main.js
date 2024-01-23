dc53ad52c5ca0b60323b30df4d86030a028e3a76
thomcsmits <thomcsmits@outlook.com>
2024-01-23 15:57:40 -0500

    feat(core): wrap data loading in map

--- main.js
+++ main.js
@@ -1,20 +1,122 @@
 import * as d3 from "d3";
 import { AnnDataSource, ObsSetsAnndataLoader } from '@vitessce/zarr';
 
-const url = 'https://assets.hubmapconsortium.org/ad693f99fb9006e68a53e97598da1509/hubmap_ui/anndata-zarr/secondary_analysis.zarr';
+const uuids = ['ad693f99fb9006e68a53e97598da1509',
+'173de2e80adf6a73ac8cff5ccce20dfc',
+'b95f34761c252ebbd1e482cd9afae73f',
+'5a5ca03fa623602d9a859224aa40ace4',
+'3c1b10bc912c60c9afc36b7423695236',
+'1dc16eb0270ff73291dd45b6a96aa3c0',
+'b05c21f9c94ce1a22a9694cd0fe0291e',
+'8cdb42ed1194255c74c8462b99bbd7ef',
+'fe0ded5fc0355c95239f9c040dd31e99',
+'367fee3b40cba682063289505b922be1',
+'b99fc30c4195958fbef217fa9ed9ec8f',
+'898138b7f45a67c574e9955fb400e9be',
+'f220c9e7bcaea3a87162cbe61287ea4d',
+'e5f7a14d93659bd0b8dc2819ffa9bc4b',
+'56cbda4789f04d79c0c3dffe21816d48',
+'0b6f63f2bd61a8c091fc7afc0f318ad1',
+'62efbe0a6abd0bcf53ab9ab29e7cd73f',
+'4b62d9d2c248323ce029859f953fdc57',
+'c81b0dc9d16eb825a7d6bce6e1b3678f',
+'5ee240959c96b49d960702755478b9fc',
+'7c9e07c96d144536525b1f889acee14d',
+'dd7ccbc306692fc5ff5e61c22845da21',
+'9a7e6be288b27ddbd3366c4ae41bbcd2',
+'018a905cdbdff684760859f594d3fd77',
+'af5741dad7aecf7960a129c3d2ae642a',
+'6e1db473492095ccc2f1393d7259b9c0',
+'fae9a1f2e7abefca2203765a3c7a5ba1',
+'8d631eee88855ac59155edca2a3bc1ca',
+'1ea6c0ac5ba60fe35bf63af8699b6fbe']
 
-const source = new AnnDataSource({ url });
-const config = {
-    url,
-    fileType: 'obsSets.anndata.zarr',
-    options: [
-    {
-        name: 'Cell Ontology Annotation',
-        path: 'obs/predicted_label'
-    }
-    ],
-};
 
-const loader = new ObsSetsAnndataLoader(source, config);
-const { data: { obsSets } } = await loader.load();
-console.log('data', obsSets)
+function getURL(uuid) {
+    return `https://assets.hubmapconsortium.org/${uuid}/hubmap_ui/anndata-zarr/secondary_analysis.zarr`;
+}
+
+const urls = uuids.map(getURL);
+
+function getLoader(url) {
+    const source = new AnnDataSource({ url });
+    const config = {
+        url,
+        fileType: 'obsSets.anndata.zarr',
+        options: [
+            {
+                name: 'Cell Ontology Annotation',
+                path: 'obs/predicted_CLID' //'obs/predicted_label'
+            }
+        ],
+    };
+
+    const loader = new ObsSetsAnndataLoader(source, config);
+    // const { data: { obsSets } } = await loader.load();
+    return loader;
+}
+
+const loaders = urls.map(getLoader);
+
+const { data: { obsSets } } = await loaders[0].load();
+console.log(obsSets)
+
+const { data: { obsSets2 } } = await loaders[1].load();
+console.log(obsSets2)
+
+// function getSomething(url) {
+//     console.log('yay')
+//     const source = new AnnDataSource({ url });
+//     console.log('nah')
+//     const config = {
+//         url,
+//         fileType: 'obsSets.anndata.zarr',
+//         options: [
+//             {
+//                 name: 'Cell Ontology Annotation',
+//                 path: 'obs/predicted_CLID' //'obs/predicted_label'
+//             }
+//         ],
+//     };
+// //     console.log('yep')
+// //     return [source, config]
+// // }
+
+
+// function getSomething(url) {
+//     const source = new AnnDataSource({ url });
+//     const config = {
+//         url,
+//         fileType: 'obsSets.anndata.zarr',
+//         options: [
+//             {
+//                 name: 'Cell Ontology Annotation',
+//                 path: 'obs/predicted_CLID' //'obs/predicted_label'
+//             }
+//         ],
+//     };
+//     loader = new ObsSetsAnndataLoader(source, config);
+//     return loader;
+// }
+
+// // first one
+// var loader = new getSomething(getURL(uuids[0]));
+// var { data: { obsSets } } = await loader.load();
+
+// console.log(obsSets)
+
+// console.log('done with first')
+// // second one
+// var loader1 = new getSomething(getURL(uuids[1]));
+// var { data: { obsSets1 } } = await loader1.load();
+// console.log(obsSets1)
+
+
+// url = getURL(uuids[1])
+
+// sourceAndConfig = getSomething(url);
+// console.log('here')
+// loader = new ObsSetsAnndataLoader(sourceAndConfig[0], sourceAndConfig[1]);
+// var { data: { obsSets } } = await loader.load();
+// console.log('second', obsSets)
+
