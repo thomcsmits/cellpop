6e6158dd381a266c54746fd12b3b8ac7844a71fe
thomcsmits <thomcsmits@outlook.com>
2024-02-28 16:50:51 -0500

    feat: add moving of rectangles and color change to animation

--- src/visualization/animation.js
+++ src/visualization/animation.js
@@ -13,27 +13,29 @@ export function showAnimation(data) {
 
 }
 
-function createStackedBar(data2) {
+// test data
+// let data2 = {
+    //     countsMatrix: [{row: 'sampleA', col: 'cellZ', value: 10},
+    //              {row: 'sampleB', col: 'cellZ', value: 5},
+    //             //  {row: 'sampleC', col: 'cellZ', value: 10},
+    //              {row: 'sampleA', col: 'cellY', value: 20},
+    //              {row: 'sampleB', col: 'cellY', value: 3},
+    //              {row: 'sampleC', col: 'cellY', value: 15},
+    //              {row: 'sampleA', col: 'cellX', value: 10},
+    //              {row: 'sampleB', col: 'cellX', value: 5},
+    //              {row: 'sampleC', col: 'cellX', value: 10},
+    //              {row: 'sampleA', col: 'cellW', value: 20},
+    //              {row: 'sampleB', col: 'cellW', value: 3},
+    //              {row: 'sampleC', col: 'cellW', value: 15},],
+    //     rowNames: ['sampleA', 'sampleB', 'sampleC'],
+    //     colNames: ['cellZ', 'cellY', 'cellX', 'cellW']
+    // }
 
-    let data = {
-        countsMatrix: [{row: 'sampleA', col: 'cellZ', value: 10},
-                 {row: 'sampleB', col: 'cellZ', value: 5},
-                //  {row: 'sampleC', col: 'cellZ', value: 10},
-                 {row: 'sampleA', col: 'cellY', value: 20},
-                 {row: 'sampleB', col: 'cellY', value: 3},
-                 {row: 'sampleC', col: 'cellY', value: 15},
-                 {row: 'sampleA', col: 'cellX', value: 10},
-                 {row: 'sampleB', col: 'cellX', value: 5},
-                 {row: 'sampleC', col: 'cellX', value: 10},
-                 {row: 'sampleA', col: 'cellW', value: 20},
-                 {row: 'sampleB', col: 'cellW', value: 3},
-                 {row: 'sampleC', col: 'cellW', value: 15},],
-        rowNames: ['sampleA', 'sampleB', 'sampleC'],
-        colNames: ['cellZ', 'cellY', 'cellX', 'cellW']
-    }
-    console.log('data', data)
-    console.log('data', data2)
+function createStackedBar(data) {
+    let width = 1000;
+    let height = 1000;
 
+    // determine the start and end of each rect for the stacked bar chart
     const rowValsCounter = [];
     for (const row of data.rowNames) {
         rowValsCounter.push({row: row, counter: 0});
@@ -47,18 +49,17 @@ function createStackedBar(data2) {
         rowValsCounter.filter(r => r.row === entry.row)[0].counter = newVal;
     }
 
-    let width = 1000;
-    let height = 1000;
-
+    // create new svg
     let svg = d3.select("#app")
         .append("svg")
-            .attr("width", width + 100)
-            .attr("height", height + 100)
+            .attr("width", 2000)
+            .attr("height", 2000)
         .append("g")
             .attr("class", "animate")
             .attr("transform", 
-                 "translate(50,50)")
+                 "translate(300,150)")
 
+    // add x axis
     let x = d3.scaleBand()
         .domain(data.rowNames)
         .range([0,width])
@@ -69,20 +70,23 @@ function createStackedBar(data2) {
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
+    // add y axis
     let y = d3.scaleLinear()
-        .domain([0, getUpperBound(stackedData.flat().flat())])
+        .domain([0, getUpperBound(rowValsCounter.map(d => d.counter))])
         .range([ height, 0 ]);
     
     svg.append("g")
         .attr("class", "axisleft")
         .call(d3.axisLeft(y));
     
+    // add color
     var color = d3.scaleOrdinal()
         .domain(data.colNames)
         .range(["#1A2A22", "#79FFFC", "#8F5D4E", "#FFFF7C", "#FFFF7C", 
                 "#C665BF", "#8AFF79", "#4E5C35", "#A4FCE5", "#FF8095", "#7A85FE"])
 
 
+    // add all rectangles
     svg.append("g").selectAll()
         .attr("class", "bars")
         .data(data.countsMatrix, function(d) {return d.row+":"+d.col;})
@@ -96,24 +100,65 @@ function createStackedBar(data2) {
             .attr("fill", function(d) {return color(d.col)})
 
 
-    // d3.select(".animate")
-    //     .transition()
-    //     .duration(2000)
-    //     // .attr("transform", "translate(500,0)")
-    //     .attr("transform", "rotate(90, 650, 750)")
+    // animations
+    // rotate
+    d3.select(".animate")
+        .transition()
+        .duration(2000)
+        .delay(2000)
+        // .attr("transform", "translate(500,0)")
+        .attr("transform", "rotate(90, 650, 750)")
+
+    // rotate x-labels the other way
+    d3.select(".animate")
+        .select(".axisbottom")
+        .selectAll("text")
+        .transition()
+        .duration(2000)
+        .delay(2000)
+        .attr("transform", "translate(-15,130)rotate(-90)")
 
     // d3.selectAll(".animate")
     //     .select("g.axisleft")
     //         .transition()
-    //         .duration(2000)
+    //         .delay(2000)
+    //         .duration(1)
     //         .remove()
+            
 
-    // d3.selectAll(".animate")
-    //     .select("g.axisbottom")
-    //         .transition()
-    //         .duration(2000)
-    //         .attr("transform", "rotate(-90, 500, 400)")
+    let y2 = d3.scaleBand()
+            .range([ height, 0 ])
+            .domain(data.colNames)
+            .padding(0.01);
+    
+    // call the new axis
+    d3.selectAll(".animate")
+        .select("g.axisleft")
+        .transition()
+        .delay(5000)
+        .call(d3.axisLeft(y2))
         
+    // move the rects
+    d3.selectAll(".animate")
+        .selectAll(".bar-rects")
+        .transition()
+        .delay(6000)
+        .ease(d3.easeLinear)
+        .attr("y", function(d) {return y2(d.col)})
+        .attr("height", y2.bandwidth())
 
-    console.log('svg', svg)
+
+    // Color axis
+	let colorRange = d3.scaleLinear()
+        .range(["white", "#69b3a2"])
+        .domain([0,2000])
+
+
+    // change the color of the rects
+    d3.selectAll(".animate")
+        .selectAll(".bar-rects")
+        .transition()
+        .delay(7500)
+        .attr("fill", function(d) {return colorRange(d.value)})
+        
 }
