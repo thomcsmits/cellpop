212e1af5806fbfd575f88a07c8ee1ad80cfd7c7b
thomcsmits <thomcsmits@outlook.com>
2024-02-28 11:54:06 -0500

    feat: add metadata options

--- src/visualization/index.js
+++ src/visualization/index.js
@@ -4,9 +4,13 @@ import { renderHeatmap } from "./heatmap";
 import { renderTopBar } from "./barTop";
 import { renderLeftBar } from "./barSide";
 import { renderGraph } from "./graph";
+import { getPossibleMetadataSelections, sortByMetadata } from "./metadata";
 
 // visualization
 export function getMainVis(data) {
+	console.log(data);
+	const app = document.getElementById('app');
+
 	// set the dimensions of the graph
 	let widthRatio = 0.9;
 	let heightRatio = 0.6; 
@@ -37,11 +41,11 @@ export function getMainVis(data) {
 	// var margin = {top: 100, right: 100, bottom: 100, left: 150};
 	let dimensions = {
 		global: {width: width, widthSplit: [widthLeft, widthRight], height: height, heightSplit: [heightTop, heightBottom]},
-		heatmap: {offsetWidth: widthLeft, offsetHeight: heightTop, width: widthRight, height: heightBottom, margin: {top: 0, right: 50, bottom: 100, left: 0}},
+		heatmap: {offsetWidth: widthLeft, offsetHeight: heightTop, width: widthRight, height: heightBottom, margin: {top: 0, right: 200, bottom: 100, left: 0}},
 		barTop: {offsetWidth: widthLeft, offsetHeight: 0, width: widthRight, height: heightTop, margin: {top: 50, right: 50, bottom: 0, left: 0}},
 		barLeft: {offsetWidth: 0, offsetHeight: heightTop, width: widthLeft, height: heightBottom, margin: {top: 0, right: 0, bottom: 100, left: 50}},
-		graph: {offsetWidth: widthLeft, offsetHeight: height, width: widthRight, height: heightTop, margin: {top: 0, right: 0, bottom: 0, left: 0}},
-		detailBar: {offsetWidth: widthLeft, offsetHeight: 0, width: widthRight, height: height, margin: {top: 50, right: 50, bottom: 50, left: 0}},
+		graph: {offsetWidth: widthLeft, offsetHeight: height, width: widthRight, height: heightTop, margin: {top: 0, right: 200, bottom: 0, left: 0}},
+		detailBar: {offsetWidth: widthLeft, offsetHeight: 0, width: widthRight, height: height, margin: {top: 50, right: 200, bottom: 50, left: 0}},
 		textSize: {title: '20px', label: '30px', tick: '10px'}
 		// barLeft: {offsetWidth: 100, offsetHeight: heightTop, width: widthLeft, height: heightBottom, margin: {top: 50, right: 0, bottom: 0, left: 100}},
 	};
@@ -52,17 +56,48 @@ export function getMainVis(data) {
 	// add a (temporary) button for switch between % and n
 	let buttonFraction = document.createElement('button');
 	buttonFraction.textContent = 'Change fraction/absolute';
-	document.body.appendChild(buttonFraction);
+	app.appendChild(buttonFraction);
 	buttonFraction.addEventListener('click', () => alert('hi'))
 
 	// add a button for resetting the bottom thing
 	let buttonReset = document.createElement('button');
 	buttonReset.textContent = 'Reset stacked bar charts';
-	document.body.appendChild(buttonReset);
+	app.appendChild(buttonReset);
 	buttonReset.addEventListener('click', () => d3.selectAll('.bardetail').remove())
 
+	// add a (temporary) button for metadata
+	const rowsMetaOptionsShown = getPossibleMetadataSelections(data);
+
+	let buttonMetadata = document.createElement('div');
+	let buttonMetadataText = document.createElement('p');
+	buttonMetadataText.innerText = "Group by metadata";
+	buttonMetadata.appendChild(buttonMetadataText);
+	for (const op of [['None', 0], ...rowsMetaOptionsShown]) {
+		let label = document.createElement("label");
+		label.innerText = op[0];
+		let input = document.createElement("input");
+		input.type = "radio";
+		input.name = "meta";
+		input.checked = false;
+		label.appendChild(input);
+		label.addEventListener('click', () => {
+			if (op[0] !== 'None') {
+				console.log(op[0])
+			}
+		})
+		buttonMetadata.appendChild(label);
+	}
+	app.appendChild(buttonMetadata);
+
+	sortByMetadata(data, ['sex', ['Female', 'Male']])
+
+	// add div for visualization
+	let mainVis = document.createElement('div');
+	mainVis.id = 'mainVis';
+	app.appendChild(mainVis);
+
 	// append the svg object to the body of the page
-	let svg = d3.select("#app")
+	let svg = d3.select("#mainVis")
 	.append("svg")
 		.attr("width", width)
 		.attr("height", height)
