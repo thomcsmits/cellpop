5b5312078691cccf8963fe2fa4a7a261dcbe5b40
thomcsmits <thomcsmits@outlook.com>
2024-03-13 20:53:34 -0400

    feat: add buttons for fraction, metadata and theme

--- src/CellPopComponent.jsx
+++ src/CellPopComponent.jsx
@@ -1,52 +1,36 @@
+
+import React, { useEffect, useState } from "react";
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 import * as d3 from "d3";
 import { getMainVis } from "./visualization";
 // import { showAnimation } from "./src/visualization/animation";
 import { loadHuBMAPData } from "./dataLoading/dataHuBMAP"; 
-import { useState } from "react";
-
-// data
-var uuids = ['ad693f99fb9006e68a53e97598da1509',
-    '173de2e80adf6a73ac8cff5ccce20dfc',
-	'b95f34761c252ebbd1e482cd9afae73f',
-	'5a5ca03fa623602d9a859224aa40ace4',
-	'3c1b10bc912c60c9afc36b7423695236',
-	'1dc16eb0270ff73291dd45b6a96aa3c0',
-	'b05c21f9c94ce1a22a9694cd0fe0291e',
-	'8cdb42ed1194255c74c8462b99bbd7ef',
-	'fe0ded5fc0355c95239f9c040dd31e99',
-	'367fee3b40cba682063289505b922be1',
-	'b99fc30c4195958fbef217fa9ed9ec8f',
-	'898138b7f45a67c574e9955fb400e9be',
-	'f220c9e7bcaea3a87162cbe61287ea4d',
-	'e5f7a14d93659bd0b8dc2819ffa9bc4b',
-	'56cbda4789f04d79c0c3dffe21816d48',
-	'0b6f63f2bd61a8c091fc7afc0f318ad1',
-	'62efbe0a6abd0bcf53ab9ab29e7cd73f',
-	'4b62d9d2c248323ce029859f953fdc57',
-	'c81b0dc9d16eb825a7d6bce6e1b3678f',
-	'5ee240959c96b49d960702755478b9fc',
-	'7c9e07c96d144536525b1f889acee14d',
-	'dd7ccbc306692fc5ff5e61c22845da21',
-	'9a7e6be288b27ddbd3366c4ae41bbcd2',
-	'018a905cdbdff684760859f594d3fd77',
-	'af5741dad7aecf7960a129c3d2ae642a',
-	'6e1db473492095ccc2f1393d7259b9c0',
-	'fae9a1f2e7abefca2203765a3c7a5ba1',
-	'8d631eee88855ac59155edca2a3bc1ca',
-	'1ea6c0ac5ba60fe35bf63af8699b6fbe']
-
-// uuids = uuids.slice(0, 3);
-// console.log(uuids.length)
+import { getPossibleMetadataSelections } from "./visualization/metadata";
 
 
 export const CellPop = (props) => {
+	console.log(props);
+	if (!props.data) {
+		return <></>;
+	}
 
 	const [theme, setTheme] = useState(props.theme);
 	const [dimensions, setDimensions] = useState(props.dimensions);
 	const [fraction, setFraction] = useState(false);
+	const [metadataField, setMetadataField] = useState("None");
+
+	
 
 
 	// get metadata options
+	const metadataFields = getPossibleMetadataSelections(props.data);
 
 
 	// useref for creating the svg
@@ -60,14 +44,63 @@ export const CellPop = (props) => {
 	
 
 	// create MUI buttons with callback functions to e.g. change theme
+	function changeTheme(event, newTheme) {
+		if (newTheme !== null) {
+			setTheme(newTheme);
+		}
+	}
+
+	function changeFraction(event, newFraction) {
+		if (newFraction !== null) {
+			setFraction(newFraction);
+		}
+	}
+
+	function changeMetadataField(event) {
+		setMetadataField(event.target.value);
+	}
 	
 	return (
 		<div>
+			<ToggleButtonGroup
+				color="primary"
+				value={fraction}
+				exclusive
+				onChange={changeFraction}
+				aria-label="Fraction"
+			>
+				<ToggleButton value={false}>Count</ToggleButton>
+				<ToggleButton value={true}>Fraction</ToggleButton>
+			</ToggleButtonGroup>
+			
+			<FormControl sx={{ m: 1, width: 300 }}>
+				<InputLabel id="sort-by-metadata">Sort by metadata</InputLabel>
+				<Select
+					labelId="demo-simple-select-label"
+					id="demo-simple-select"
+					value={metadataField}
+					label="select-metadata"
+					onChange={changeMetadataField}
+				>
+					<MenuItem value="None" key="None">None</MenuItem>
+					{metadataFields.map(d => {
+						return <MenuItem value={d[0]} key={d[0]}>{d[0]}</MenuItem>
+					})}
+				</Select>
+			</FormControl>
 
+			<ToggleButtonGroup
+				color="primary"
+				value={theme}
+				exclusive
+				onChange={changeTheme}
+				aria-label="Theme"
+			>
+				<ToggleButton value="light">Light</ToggleButton>
+				<ToggleButton value="dark">Dark</ToggleButton>
+			</ToggleButtonGroup>
 		</div>
 	)
-		
-	// return <div id="app"></div>
 }
 
 // loadHuBMAPData(uuids).then((data) => {
