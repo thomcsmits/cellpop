ed03243629baa46ce8982a8001061deac410c2aa
thomcsmits <thomcsmits@outlook.com>
2024-03-13 21:39:33 -0400

    feat: only set dimensions if not given

--- src/visualization/index.js
+++ src/visualization/index.js
@@ -7,52 +7,55 @@ import { renderGraph } from "./graph";
 import { getPossibleMetadataSelections, sortByMetadata } from "./metadata";
 
 // visualization
-export function getMainVis(data) {
+export function getMainVis(data, dimensions=null) {
+	if (!dimensions) {
+		// set the dimensions of the graph
+		let widthRatio = 0.9;
+		let heightRatio = 0.8; 
+
+		let widthRight = data.colNames.length * 25;
+		let heightBottom =  data.rowNames.length * 40;
+
+		let width = widthRight / widthRatio;
+		let height = heightBottom / heightRatio;
+
+		let widthLeft = width - widthRight;
+		let heightTop = height - heightBottom;
+
+		// height = height + 400;
+
+		// let widthFull = 800;
+		// let heightFull = 800;
+		// let widthLeft = 200;
+		// let widthRight = 600;
+		// let heightTop = 200;
+		// let heightBottom = 600;
+
+		// let height = heightFull;
+		// let width = widthFull;
+
+		// var width = data.countsMatrix.length * 5;
+		// var height = data.counts.length * 20;
+		// var margin = {top: 100, right: 100, bottom: 100, left: 150};
+		let dimensions = {
+			global: {width: width, widthSplit: [widthLeft, widthRight], height: height, heightSplit: [heightTop, heightBottom]},
+			heatmap: {offsetWidth: widthLeft, offsetHeight: heightTop, width: widthRight, height: heightBottom, margin: {top: 0, right: 200, bottom: 100, left: 0}},
+			barTop: {offsetWidth: widthLeft, offsetHeight: 0, width: widthRight, height: heightTop, margin: {top: 50, right: 50, bottom: 0, left: 0}},
+			barLeft: {offsetWidth: 0, offsetHeight: heightTop, width: widthLeft, height: heightBottom, margin: {top: 0, right: 0, bottom: 100, left: 50}},
+			graph: {offsetWidth: widthLeft, offsetHeight: height, width: widthRight, height: heightTop, margin: {top: 0, right: 200, bottom: 0, left: 0}},
+			detailBar: {offsetWidth: widthLeft, offsetHeight: 0, width: widthRight, height: height, margin: {top: 50, right: 200, bottom: 50, left: 0}},
+			textSize: {title: '20px', label: '30px', tick: '10px'}
+			// barLeft: {offsetWidth: 100, offsetHeight: heightTop, width: widthLeft, height: heightBottom, margin: {top: 50, right: 0, bottom: 0, left: 100}},
+		};
+		
+		// var dimensionsHeatmap = {width: widthRight, height: heightBottom, margin: {top: 50, right: 50, bottom: 50, left: 50}}
+		// var marginHeatmap = {top: 100, right: 100, bottom: 100, left: 150};
+	}
+
 	console.log('data', data);
 	const app = document.getElementById('app');
 
-	// set the dimensions of the graph
-	let widthRatio = 0.9;
-	let heightRatio = 0.8; 
-
-	let widthRight = data.colNames.length * 25;
-	let heightBottom =  data.rowNames.length * 40;
-
-	let width = widthRight / widthRatio;
-	let height = heightBottom / heightRatio;
-
-	let widthLeft = width - widthRight;
-	let heightTop = height - heightBottom;
-
-	// height = height + 400;
-
-	// let widthFull = 800;
-	// let heightFull = 800;
-	// let widthLeft = 200;
-	// let widthRight = 600;
-	// let heightTop = 200;
-	// let heightBottom = 600;
-
-	// let height = heightFull;
-	// let width = widthFull;
-
-	// var width = data.countsMatrix.length * 5;
-	// var height = data.counts.length * 20;
-	// var margin = {top: 100, right: 100, bottom: 100, left: 150};
-	let dimensions = {
-		global: {width: width, widthSplit: [widthLeft, widthRight], height: height, heightSplit: [heightTop, heightBottom]},
-		heatmap: {offsetWidth: widthLeft, offsetHeight: heightTop, width: widthRight, height: heightBottom, margin: {top: 0, right: 200, bottom: 100, left: 0}},
-		barTop: {offsetWidth: widthLeft, offsetHeight: 0, width: widthRight, height: heightTop, margin: {top: 50, right: 50, bottom: 0, left: 0}},
-		barLeft: {offsetWidth: 0, offsetHeight: heightTop, width: widthLeft, height: heightBottom, margin: {top: 0, right: 0, bottom: 100, left: 50}},
-		graph: {offsetWidth: widthLeft, offsetHeight: height, width: widthRight, height: heightTop, margin: {top: 0, right: 200, bottom: 0, left: 0}},
-		detailBar: {offsetWidth: widthLeft, offsetHeight: 0, width: widthRight, height: height, margin: {top: 50, right: 200, bottom: 50, left: 0}},
-		textSize: {title: '20px', label: '30px', tick: '10px'}
-		// barLeft: {offsetWidth: 100, offsetHeight: heightTop, width: widthLeft, height: heightBottom, margin: {top: 50, right: 0, bottom: 0, left: 100}},
-	};
 	
-	// var dimensionsHeatmap = {width: widthRight, height: heightBottom, margin: {top: 50, right: 50, bottom: 50, left: 50}}
-	// var marginHeatmap = {top: 100, right: 100, bottom: 100, left: 150};
-
 	// add a (temporary) button for switch between % and n
 	let buttonFractionOn = document.createElement('button');
 	buttonFractionOn.textContent = 'Change to fraction';
