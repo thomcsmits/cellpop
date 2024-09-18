a1c1dfb891f618a73784a965e94c457553dd430a
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-09-18 13:04:36 -0400

    legend cleanup

--- src/visx-visualization/Legend.tsx
+++ src/visx-visualization/Legend.tsx
@@ -12,23 +12,42 @@ export default function Legend() {
   const { theme } = useCellPopTheme();
   const domain = colors.domain();
   const id = useId() + "-legend";
+
+  const adjustedWidth = width - 40; // 20px padding on each side
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
       <label htmlFor={id} style={{ color: theme.text }}>
         Counts
       </label>
-      <svg width={width} id={id}>
+      <svg width={adjustedWidth} id={id}>
         <defs>
           <linearGradient id="legendGradient" gradientTransform="rotate(0)">
             <stop offset="5%" stopColor={rgbToHex(colors(domain[0]))} />
             <stop offset="95%" stopColor={rgbToHex(colors(domain[1]))} />
           </linearGradient>
         </defs>
-        <rect width={width} height={20} y={20} fill="url(#legendGradient)" />
-        <text y={36} x={40} fill={theme.text}>
+        <rect
+          width={adjustedWidth}
+          height={20}
+          y={20}
+          fill="url(#legendGradient)"
+          rx={5}
+          ry={5}
+          stroke={theme.text}
+          strokeWidth={2}
+        />
+        <text y={36} x={8} fill={theme.text}>
           0
         </text>
-        <text y={36} x={width - 40} fill={theme.text}>
+        <text y={36} x={adjustedWidth - 8} textAnchor="end" fill={theme.text}>
           {maxCount}
         </text>
       </svg>
