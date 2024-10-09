ea3233f092afd91a58c7450e54f451af315214d7
thomcsmits <thomcsmits@outlook.com>
2024-10-09 09:31:15 -0400

    simplify

--- python/js/widget.tsx
+++ python/js/widget.tsx
@@ -3,26 +3,12 @@ import { createRender, useModelState } from "@anywidget/react";
 import * as cellpop from "cellpop";
 // test
 const render = createRender(() => {
-	const [value, setValue] = useModelState<number>("value");
-    let [data, setData] = React.useState(null);
 
-      React.useEffect(() => {
-        cellpop.loadHuBMAPData(["018a905cdbdff684760859f594d3fd77"]).then(setData);
-          console.log("data has loaded");
-      }, [])
-    
-    console.log("data", data);
-
-    const dimensions = cellpop.getDimensions();
-    console.log("dimensions", dimensions);
-
+    console.log("rendered")
 	return (
 		<div className="cellpop">
-			<button onClick={() => setValue(value + 1)}>
-				count is {value}
-			</button>
-
-            {/* {data ? <cellpop.CellPop data={data} theme={"light"} dimensions={dimensions} /> : null} */}
+			<button>hello</button>
+            <cellpop.CellPop />
 		</div>
 	);
 });
