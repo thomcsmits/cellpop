8acee932551b8b3bc960d56d7c9221b511c93866
thomcsmits <thomcsmits@outlook.com>
2024-10-23 15:14:09 -0400

    update widget for visx

--- python/js/widget.tsx
+++ python/js/widget.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
 import * as cellpop from "cellpop";
+import * as React from "react";
 
 // test
 const render = createRender(() => {
@@ -10,15 +10,28 @@ const render = createRender(() => {
       React.useEffect(() => {
         cellpop.loadHuBMAPData(uuids).then(setData);
       }, [uuids])
-    
-    console.log("data", data);
-
-    const dimensions = cellpop.getDimensions();
-    console.log("dimensions", dimensions);
 
 	return (
 		<div className="cellpop">
-            {data ? <cellpop.CellPop data={data} theme={"light"} dimensions={dimensions} /> : null}
+      {data ? 
+        <cellpop.CellPop
+          data={data}
+          theme={"light"}
+          dimensions={{ width: 1000, height: 700 }}
+          yAxisConfig={{
+            label: "Sample",
+            createHref: (row) =>
+              `https://portal.hubmapconsortium.org/browse/${row}`,
+            flipAxisPosition: true,
+          }}
+          xAxisConfig={{
+            label: "Cell Type",
+            createHref: (col) =>
+              `https://www.ebi.ac.uk/ols4/search?q=${col}&ontology=cl`,
+            flipAxisPosition: true,
+          }}
+        />
+      : null}
 		</div>
 	);
 });
