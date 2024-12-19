6b8b34888bd5ab4d4bc6e714a8c67d60283dd514
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-12-19 11:35:23 -0500

    Try `build-demo` config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,38 @@
-import path from "path";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, UserConfigFnObject } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "cellpop",
-      fileName: (format) => `index.${format}.js`
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+export default defineConfig(({ mode }) => {
+  if (mode === "demo") {
+    return {
+      build: {
+        rollupOptions: {
+          input: {
+            "index.html": path.resolve(__dirname, "index.html"),
+          },
+        },
+      },
+      base: "/cellpop/",
+    };
+  }
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "cellpop",
+        fileName: (format) => `index.${format}.js`,
+      },
+      rollupOptions: {
+        external: ["react", "react-dom"],
+        output: {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
         },
       },
     },
-  },
-  base: "/cellpop/",
-});
+    base: "/cellpop/",
+  };
+}) satisfies UserConfigFnObject;
